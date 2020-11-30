3caaa97aa96bdc3d55be72e81082f80f50c9b324
ShawnGao <2519306859@qq.com>
2020-11-30 19:39:25 +0800

    Update Error Code U0001 Out of INT DATASIZE

--- Fibonacci/src/club/shawngao/fibonacci/Main.java
+++ Fibonacci/src/club/shawngao/fibonacci/Main.java
@@ -4,18 +4,30 @@ import java.util.Scanner;
 
 public class Main {
 
+    /**
+     * 输出指定元素个数的斐波那契数列
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num;
         int i;
-        System.out.print("输入想输出斐波那契数列的元素个数：");
+        System.out.print("输入想输出斐波那契数列的元素个数(num < 92)：");
         num = sc.nextInt();
-        for(i = 1; i <= num; i++) {
-            System.out.printf("%20d", fibonacci(i));
-            if(i % 5 == 0)
-                System.out.println(" ");
+        if(num < 92) {
+            for (i = 1; i <= num; i++) {
+                System.out.printf("%20d", fibonacci(i));
+                if (i % 5 == 0)
+                    System.out.println(" ");
+            }
+        }
+        else {
+            System.out.print("\n\033[31m U0001 Out of INT DATASIZE\n");
         }
     }
+
+    /**
+     * 使用递归来计算斐波那契的项
+     */
     private static int fibonacci(int number) {
         if(number == 1 || number == 2)
             return 1;
