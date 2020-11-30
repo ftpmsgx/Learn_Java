11a197eea3952215cc7c2150d760a023f6066450
ShawnGao <2519306859@qq.com>
2020-11-30 10:58:08 +0800

    Fibonacci

---
+++ Fibonacci/src/club/shawngao/fibonacci/Main.java
@@ -0,0 +1,25 @@
+package club.shawngao.fibonacci;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        int i;
+        System.out.print("输入想输出斐波那契数列的元素个数：");
+        num = sc.nextInt();
+        for(i = 1; i <= num; i++) {
+            System.out.printf("%20d", fibonacci(i));
+            if(i % 5 == 0)
+                System.out.println(" ");
+        }
+    }
+    private static int fibonacci(int number) {
+        if(number == 1 || number == 2)
+            return 1;
+        else
+            return fibonacci(number - 1) + fibonacci(number - 2);
+    }
+}
