10896f491f433d83a6962013c2e0aa880a532a7d
ShawnGao <2519306859@qq.com>
2020-12-10 19:16:01 +0800

    RPN

---
+++ RPN/src/club/shawngao/rpn/Main.java
@@ -0,0 +1,40 @@
+package club.shawngao.rpn;
+
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] s = { "2", "1", "+", "10", "*", "3", "%" }; // 逆波兰式
+
+        String operator = "+-%*/";
+        Stack<Double> stack = new Stack<Double>(); // 栈
+        Double a, b;
+        int i;
+        for (i = 0; i < s.length; i++) {
+            if (!operator.contains(s[i]))
+                stack.push(Double.parseDouble(s[i]));
+            else {
+                a = stack.pop();
+                b = stack.pop();
+                switch (operator.indexOf(s[i])) {
+                    case 0:
+                        stack.push(a + b);
+                        break;
+                    case 1:
+                        stack.push(a - b);
+                        break;
+                    case 2:
+                        stack.push(a % b);
+                        break;
+                    case 3:
+                        stack.push(a * b);
+                        break;
+                    case 4:
+                        stack.push(a / b);
+                        break;
+                }
+            }
+        }
+        System.out.println("result = " + stack.pop());
+    }
+}
